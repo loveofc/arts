29b7472f385a670f9bbcb65f142c3801fadf6a1f
용환 <lyhlyhlyhlyhlyh@daum.net>
2022-10-11 17:18:53 +0900

    c

---
+++ pro05/src/main/java/sec01/ex01/SecondServlet.java
@@ -0,0 +1,31 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecondServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet SecondServlet");
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>hi SecondServlet</h1>");
+		
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy SecondServlet");
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init SecondServlet");
+	}
+}
---
+++ pro05/src/main/java/sec01/ex01/ThirdServlet.java
@@ -0,0 +1,51 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ccc")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init ThirdServlet");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy ThirdServlet");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("service ThirdServlet");
+		doGet(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet ThirdServlet");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>hi ThirdServlet</h1>");
+	}
+
+}
